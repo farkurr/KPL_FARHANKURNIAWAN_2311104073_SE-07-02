295f56c5d594d7517d3fb3cfbe44334597a5e9e5
USERNAME_GITHUB_YANG_BENAR <EMAIL_GITHUB_YANG_BENAR>
2025-03-11 01:29:55 +0700

    ngumpulin TP4

---
+++ 04_Automata_dan_Table-Driven_Construction/TP 4/DoorMachine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class DoorMachine
+{
+    private enum State { Terkunci, Terbuka }
+    private State state;
+
+    public DoorMachine()
+    {
+        state = State.Terkunci;
+        Console.WriteLine("Pintu terkunci");
+    }
+
+    public void Unlock()
+    {
+        if (state == State.Terkunci)
+        {
+            state = State.Terbuka;
+            Console.WriteLine("Pintu tidak terkunci");
+        }
+    }
+
+    public void Lock()
+    {
+        if (state == State.Terbuka)
+        {
+            state = State.Terkunci;
+            Console.WriteLine("Pintu terkunci");
+        }
+    }
+}
---
+++ 04_Automata_dan_Table-Driven_Construction/TP 4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Program berhasil dijalankan!");
+
+        // Demonstrasi Table-Driven
+        Console.WriteLine("Kode Pos Batununggal: " + KodePos.GetKodePos("Batununggal"));
+        Console.WriteLine("Kode Pos Maleer: " + KodePos.GetKodePos("Maleer"));
+
+        // Demonstrasi State-Based Construction
+        DoorMachine pintu = new DoorMachine();
+        pintu.Unlock();
+        pintu.Lock();
+    }
+}
