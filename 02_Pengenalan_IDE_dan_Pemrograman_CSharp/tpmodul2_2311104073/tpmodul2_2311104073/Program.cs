67ae82d7efbcf9503ed8557ccfc78bd53efbf965
USERNAME_GITHUB_YANG_BENAR <EMAIL_GITHUB_YANG_BENAR>
2025-03-10 14:50:14 +0700

    upload ulang folder

---
+++ 02_Pengenalan_IDE_dan_Pemrograman_CSharp/tpmodul2_2311104073/tpmodul2_2311104073/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Menerima input satu karakter
+        Console.Write("Masukkan satu huruf: ");
+        char huruf = char.ToUpper(Console.ReadKey().KeyChar);
+        Console.WriteLine(); // Pindah baris setelah input
+
+        // Mengecek apakah huruf vokal atau konsonan
+        if ("AIUEO".Contains(huruf))
+        {
+            Console.WriteLine($"Huruf {huruf} merupakan huruf vokal");
+        }
+        else
+        {
+            Console.WriteLine($"Huruf {huruf} merupakan huruf konsonan");
+        }
+
+        // Membuat array bilangan genap
+        int[] bilanganGenap = { 2, 4, 6, 8, 10 };
+
+        // Menampilkan isi array
+        for (int i = 0; i < bilanganGenap.Length; i++)
+        {
+            Console.WriteLine($"Angka genap {i + 1} : {bilanganGenap[i]}");
+        }
+    }
+}
