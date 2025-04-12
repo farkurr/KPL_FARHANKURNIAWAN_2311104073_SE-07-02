951d9b22103fdee5f16b43aefe6569bc7011cc1f
USERNAME_GITHUB_YANG_BENAR <EMAIL_GITHUB_YANG_BENAR>
2025-04-12 14:28:39 +0700

    menambahkan dua class SayaTubeVideo dan SayaTubeUser

---
+++ 06_Design_by_Contract_dan_Defensive_Programming/modul6_2311104073/modul6_2311104073/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        SayaTubeUser user = new SayaTubeUser("Farhan");
+
+        for (int i = 1; i <= 10; i++)
+        {
+            SayaTubeVideo video = new SayaTubeVideo($"Review Film Ke-{i} oleh Farhan");
+            video.IncreasePlayCount(1000000); // test play count
+            user.AddVideo(video);
+        }
+
+        user.PrintAllVideoPlaycount();
+        Console.WriteLine($"Total Play Count: {user.GetTotalVideoPlayCount()}");
+    }
+}
---
+++ 06_Design_by_Contract_dan_Defensive_Programming/modul6_2311104073/modul6_2311104073/SayaTubeVideo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playCount;
+
+    public SayaTubeVideo(string title)
+    {
+        if (string.IsNullOrEmpty(title) || title.Length > 200) throw new ArgumentException("Judul invalid");
+        this.title = title;
+        Random rnd = new Random();
+        this.id = rnd.Next(10000, 99999);
+        this.playCount = 0;
+    }
+
+    public void IncreasePlayCount(int count)
+    {
+        if (count < 0 || count > 25000000) throw new ArgumentException("Play count tidak valid");
+        checked
+        {
+            this.playCount += count;
+        }
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine($"ID: {id}, Title: {title}, Play Count: {playCount}");
+    }
+
+    public int GetPlayCount()
+    {
+        return this.playCount;
+    }
+
+    public string GetTitle()
+    {
+        return this.title;
+    }
+}
