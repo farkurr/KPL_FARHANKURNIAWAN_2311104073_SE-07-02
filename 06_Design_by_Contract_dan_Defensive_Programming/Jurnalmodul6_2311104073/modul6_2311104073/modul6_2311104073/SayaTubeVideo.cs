5b169aa483cba08bd819d999eb6eb2776fc737c1
USERNAME_GITHUB_YANG_BENAR <EMAIL_GITHUB_YANG_BENAR>
2025-04-12 18:01:40 +0700

    menambahkan dua class SayaTubeVideo dan SayaTubeUser

---
+++ 06_Design_by_Contract_dan_Defensive_Programming/Jurnalmodul6_2311104073/modul6_2311104073/modul6_2311104073/SayaTubeVideo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playCount;
+
+    public SayaTubeVideo(string title)
+    {
+        if (title == null || title.Length > 200)
+            throw new ArgumentException("Judul video invalid");
+
+        this.id = new Random().Next(10000, 99999);
+        this.title = title;
+        this.playCount = 0;
+    }
+
+    public void IncreasePlayCount(int count)
+    {
+        if (count < 0 || count > 25000000)
+            throw new ArgumentOutOfRangeException("Jumlah play count invalid");
+
+        checked
+        {
+            playCount += count;
+        }
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine($"ID: {id}");
+        Console.WriteLine($"Title: {title}");
+        Console.WriteLine($"Play Count: {playCount}");
+    }
+
+    public int GetPlayCount() => playCount;
+    public string GetTitle() => title;
+}
