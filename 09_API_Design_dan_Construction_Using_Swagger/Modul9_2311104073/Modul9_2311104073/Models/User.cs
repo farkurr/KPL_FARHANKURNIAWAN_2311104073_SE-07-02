7167fd26f5021acf2489124ffc0f907742eec36c
USERNAME_GITHUB_YANG_BENAR <EMAIL_GITHUB_YANG_BENAR>
2025-04-25 10:33:56 +0700

    Menambahkan Guided Modul 9

---
+++ 09_API_Design_dan_Construction_Using_Swagger/Modul9_2311104073/Modul9_2311104073/Models/User.cs
@@ -0,0 +1,9 @@
+﻿public class User
+{
+    public int Id { get; set; }
+    public int id { get; internal set; }
+    public string Name { get; set; }
+    public string name { get; internal set; }
+    public string Email { get; set; }
+    public string email { get; internal set; }
+}
