480127b62611b4236390ab5c9254f9f151500cf6
USERNAME_GITHUB_YANG_BENAR <EMAIL_GITHUB_YANG_BENAR>
2025-05-15 08:52:56 +0700

    Menghapus file

--- 09_API_Design_dan_Construction_Using_Swagger/Modul9_2311104073/Modul9_2311104073/Models/User.cs
+++
@@ -1,9 +0,0 @@
-﻿public class User
-{
-    public int Id { get; set; }
-    public int id { get; internal set; }
-    public string Name { get; set; }
-    public string name { get; internal set; }
-    public string Email { get; set; }
-    public string email { get; internal set; }
-}
