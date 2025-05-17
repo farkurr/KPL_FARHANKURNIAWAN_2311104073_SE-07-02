22974856b53065daf08066aa70f6f09ab9faf3ac
USERNAME_GITHUB_YANG_BENAR <EMAIL_GITHUB_YANG_BENAR>
2025-05-18 02:24:58 +0700

    Menambahkan TP Modul 10

---
+++ 10_Library_Construction/TP/tpmodul10_2311104073/AljabarLibraries/Aljabar.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace AljabarLibraries
+{
+    public class Aljabar
+    {
+        // Fungsi untuk mencari akar-akar dari persamaan kuadrat
+        public static double[] AkarPersamaanKuadrat(double[] persamaan)
+        {
+            double a = persamaan[0];
+            double b = persamaan[1];
+            double c = persamaan[2];
+            double diskriminan = b * b - 4 * a * c;
+
+            if (diskriminan < 0)
+                return new double[] { }; // tidak ada akar real
+
+            double akar1 = (-b + Math.Sqrt(diskriminan)) / (2 * a);
+            double akar2 = (-b - Math.Sqrt(diskriminan)) / (2 * a);
+            return new double[] { akar1, akar2 };
+        }
+
+        // Fungsi untuk mendapatkan hasil kuadrat dari persamaan berpangkat 1
+        public static double[] HasilKuadrat(double[] persamaan)
+        {
+            double a = persamaan[0];
+            double b = persamaan[1];
+
+            double hasilA = a * a;
+            double hasilB = 2 * a * b;
+            double hasilC = b * b;
+
+            return new double[] { hasilA, hasilB, hasilC };
+        }
+    }
+}
---
+++ 10_Library_Construction/TP/tpmodul10_2311104073/MainApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using AljabarLibraries;
+
+namespace MainApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Contoh pemanggilan HasilKuadrat
+            double[] kuadrat = Aljabar.HasilKuadrat(new double[] { 2, -3 });
+            Console.WriteLine("Hasil Kuadrat:");
+            foreach (var item in kuadrat)
+                Console.Write(item + " ");
+            Console.WriteLine();
+
+            // Contoh pemanggilan AkarPersamaanKuadrat
+            double[] akar = Aljabar.AkarPersamaanKuadrat(new double[] { 1, -3, -10 });
+            Console.WriteLine("Akar Persamaan Kuadrat:");
+            foreach (var item in akar)
+                Console.Write(item + " ");
+            Console.WriteLine();
+        }
+    }
+}
