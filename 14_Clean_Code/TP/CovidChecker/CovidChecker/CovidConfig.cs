478b2a6a43c1aa044b30298feb7e7fd61e768594
USERNAME_GITHUB_YANG_BENAR <EMAIL_GITHUB_YANG_BENAR>
2025-06-01 23:58:57 +0700

    Menambahkan tp modul 14 dan lapraknya

---
+++ 14_Clean_Code/TP/CovidChecker/CovidChecker/CovidConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+public class CovidConfig
+{
+    public string SatuanSuhu { get; set; } = "celcius";
+    public int BatasHariDemam { get; set; } = 14;
+    public string PesanDitolak { get; set; } = "Anda tidak diperbolehkan masuk ke dalam gedung ini";
+    public string PesanDiterima { get; set; } = "Anda dipersilahkan untuk masuk ke dalam gedung ini";
+
+    private const string ConfigPath = "covid_config.json";
+
+    public void LoadConfig()
+    {
+        if (File.Exists(ConfigPath))
+        {
+            string jsonString = File.ReadAllText(ConfigPath);
+            CovidConfig? config = JsonSerializer.Deserialize<CovidConfig>(jsonString);
+
+            if (config != null)
+            {
+                SatuanSuhu = config.SatuanSuhu;
+                BatasHariDemam = config.BatasHariDemam;
+                PesanDitolak = config.PesanDitolak;
+                PesanDiterima = config.PesanDiterima;
+            }
+        }
+    }
+
+    public void UbahSatuan()
+    {
+        SatuanSuhu = SatuanSuhu == "celcius" ? "fahrenheit" : "celcius";
+    }
+}
---
+++ 14_Clean_Code/TP/CovidChecker/CovidChecker/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CovidConfig config = new CovidConfig();
+        config.LoadConfig();
+        config.UbahSatuan();
+
+        Console.Write($"Berapa suhu badan Anda saat ini? Dalam nilai {config.SatuanSuhu}: ");
+        double suhu = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Berapa hari yang lalu (perkiraan) Anda terakhir memiliki gejala demam? ");
+        int hariDemam = Convert.ToInt32(Console.ReadLine());
+
+        bool suhuNormal = false;
+
+        if (config.SatuanSuhu == "celcius")
+        {
+            suhuNormal = suhu >= 36.5 && suhu <= 37.5;
+        }
+        else
+        {
+            suhuNormal = suhu >= 97.7 && suhu <= 99.5;
+        }
+
+        if (suhuNormal && hariDemam < config.BatasHariDemam)
+        {
+            Console.WriteLine(config.PesanDiterima);
+        }
+        else
+        {
+            Console.WriteLine(config.PesanDitolak);
+        }
+    }
+}
