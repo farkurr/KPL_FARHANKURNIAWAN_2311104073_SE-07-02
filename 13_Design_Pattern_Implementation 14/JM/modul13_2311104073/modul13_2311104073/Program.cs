684f4c0f687ffb996c5867ebb13a540f8a22c340
USERNAME_GITHUB_YANG_BENAR <EMAIL_GITHUB_YANG_BENAR>
2025-06-01 23:19:11 +0700

    Menambahkan JM 13 dan lapraknya

---
+++ 13_Design_Pattern_Implementation 14/JM/modul13_2311104073/modul13_2311104073/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // A. Buat 2 variabel
+        var data1 = PusatDataSingleton.GetDataSingleton();
+        var data2 = PusatDataSingleton.GetDataSingleton();
+
+        // B. Tambah data di data1
+        data1.AddSebuahData("Farhan Kurniawan");
+        data1.AddSebuahData("Bagus Prakoso");
+        data1.AddSebuahData("Asisten: Rifqi");
+
+        // C. Print isi data2
+        Console.WriteLine("Isi data2 sebelum dihapus:");
+        data2.PrintSemuaData();
+
+        // D. Hapus nama asisten dari data2
+        data2.HapusSebuahData(2);
+
+        // E. Print ulang isi data1
+        Console.WriteLine("Isi data1 setelah penghapusan:");
+        data1.PrintSemuaData();
+
+        // F. Print jumlah data
+        Console.WriteLine("Jumlah data pada data1: " + data1.GetSemuaData().Count);
+        Console.WriteLine("Jumlah data pada data2: " + data2.GetSemuaData().Count);
+    }
+}
