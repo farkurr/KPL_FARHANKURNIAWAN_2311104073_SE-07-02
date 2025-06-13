2a3cb131f4cd9e1c47fbc28e018935b55c96f521
USERNAME_GITHUB_YANG_BENAR <EMAIL_GITHUB_YANG_BENAR>
2025-06-13 22:11:23 +0700

    menghapus

--- 04_Automata_dan_Table-Driven_Construction/TP/tpmodul4_2311104073/tpmodul4_2311104073/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿class Program
-{
-    static void Main(string[] args)
-    {
-        KodePos kp = new KodePos();
-        Console.WriteLine("Kode Pos Batununggal: " + kp.getKodePos("Batununggal"));
-        Console.WriteLine("Kode Pos Maleer: " + kp.getKodePos("Maleer"));
-    }
-}
