80bbd41ea017ec0b919964b1f0bb4e61d1ea97f9
USERNAME_GITHUB_YANG_BENAR <EMAIL_GITHUB_YANG_BENAR>
2025-06-13 22:01:36 +0700

    Mengumpulkan JM Modul 2 dan lapraknya

---
+++ 02_Pengenalan_IDE_dan_Pemrograman_CSharp/JM/modul2_2311104073/modul2_2311104073/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace modul2_2311104073
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Bagian A: Input Nama
+            Console.Write("Masukkan nama Anda: ");
+            string nama = Console.ReadLine();
+            Console.WriteLine($"Selamat datang, {nama}!");
+
+            // Bagian B: Array dengan aturan print khusus
+            int[] data = new int[50];
+            for (int i = 0; i < data.Length; i++)
+            {
+                data[i] = i;
+                if (i % 6 == 0 && i != 0)
+                    Console.WriteLine($"{i} #$#$");
+                else if (i % 2 == 0 && i != 0)
+                    Console.WriteLine($"{i} ##");
+                else if (i % 3 == 0)
+                    Console.WriteLine($"{i} $$");
+                else
+                    Console.WriteLine($"{i}");
+            }
+
+            // Bagian C: Input angka & cek bilangan prima
+            Console.Write("Masukkan sebuah angka (1-10000): ");
+            string inputAngka = Console.ReadLine();
+            int nilaiInt = Convert.ToInt32(inputAngka);
+
+            if (IsPrima(nilaiInt))
+                Console.WriteLine($"Angka {nilaiInt} merupakan bilangan prima");
+            else
+                Console.WriteLine($"Angka {nilaiInt} bukan merupakan bilangan prima");
+        }
+
+        static bool IsPrima(int angka)
+        {
+            if (angka <= 1) return false;
+            for (int i = 2; i <= Math.Sqrt(angka); i++)
+            {
+                if (angka % i == 0) return false;
+            }
+            return true;
+        }
+    }
+}
