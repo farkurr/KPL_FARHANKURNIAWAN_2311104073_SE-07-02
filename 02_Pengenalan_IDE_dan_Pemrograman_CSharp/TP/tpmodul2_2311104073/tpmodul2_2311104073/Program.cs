eeeb4392369458a6d46f04d448ed4bfb95586258
USERNAME_GITHUB_YANG_BENAR <EMAIL_GITHUB_YANG_BENAR>
2025-06-13 21:49:11 +0700

    Menambahkan TP modul2 dan lapraknya

---
+++ 02_Pengenalan_IDE_dan_Pemrograman_CSharp/TP/tpmodul2_2311104073/tpmodul2_2311104073/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Bagian A - Input huruf dan cek vokal atau konsonan
+        Console.Write("Masukkan satu huruf: ");
+        char huruf = Convert.ToChar(Console.ReadLine());
+
+        if ("AIUEO".Contains(huruf))
+        {
+            Console.WriteLine($"Huruf {huruf} merupakan huruf vokal");
+        }
+        else
+        {
+            Console.WriteLine($"Huruf {huruf} merupakan huruf konsonan");
+        }
+
+        // Bagian B - Array bilangan genap
+        int[] bilanganGenap = { 2, 4, 6, 8, 10 };
+        for (int i = 0; i < bilanganGenap.Length; i++)
+        {
+            Console.WriteLine($"Angka genap {i + 1} : {bilanganGenap[i]}");
+        }
+    }
+}
