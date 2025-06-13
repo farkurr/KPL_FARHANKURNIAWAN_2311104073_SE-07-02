ae4ac6acde8a8eb7f2e1471e520c58e3d27e47fb
USERNAME_GITHUB_YANG_BENAR <EMAIL_GITHUB_YANG_BENAR>
2025-06-13 22:15:29 +0700

    menambahkan implementasi dengan table-driven

---
+++ 04_Automata_dan_Table-Driven_Construction/TP/tpmodul14_2311104073/tpmodul14_2311104073/Program.cs
@@ -0,0 +1,9 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        KodePos kp = new KodePos();
+        Console.WriteLine("Kode Pos Batununggal: " + kp.getKodePos("Batununggal"));
+        Console.WriteLine("Kode Pos Maleer: " + kp.getKodePos("Maleer"));
+    }
+}
